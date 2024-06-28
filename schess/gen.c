1761f5e83fde72841030e423c05dd087db947e2a
kiliac03 <kiliac03@mi.fu-berlin.de>
2024-06-28 14:38:41 +0200

    castling and enpassant

--- schess/gen.c
+++ schess/gen.c
@@ -53,16 +53,25 @@ log_bit(bitboard board)
 
 
 static inline void
+move_buffer_append_move(square from, square to, piece_type capture, enum MOVE_TYPE type, struct move_buffer *out)
+{
+  out->moves[out->size].from    = from;
+  out->moves[out->size].to      = to;
+  out->moves[out->size].capture = capture;
+  out->moves[out->size].type    = type;
+  ++out->size;
+}
+
+static inline void
 move_buffer_append_attacks(bitboard attacks, square from, piece_type types[NUM_SQUARES], struct move_buffer *out)
 {
+  square to;
+
   while (attacks)
   {
-    square to = pop_bit(&attacks);
-    out->moves[out->size].from = from;
-    out->moves[out->size].to = to;
-    out->moves[out->size].capture = types[to];
-    out->moves[out->size].type = MT_NORMAL;
-    ++out->size;
+    to = pop_bit(&attacks);
+
+    move_buffer_append_move(from, to, types[to], MT_NORMAL, out);
   }
 }
 
@@ -97,7 +106,7 @@ generate_knight_moves(bitboard own, bitboard other, square sq, piece_type types[
 }
 
 static inline int
-is_square_checked(bitboard own, bitboard other, bitboard other_pieces[6], square sq, piece_type types[NUM_SQUARES])
+is_square_checked(bitboard own, bitboard other, bitboard other_pieces[6], bitboard other_pawn_attacks, square sq, piece_type types[NUM_SQUARES])
 {
   bitboard attacker;
   bitboard occ = own | other;
@@ -105,21 +114,32 @@ is_square_checked(bitboard own, bitboard other, bitboard other_pieces[6], square
   attacker  = rook_attacks(occ, sq)   & (other_pieces[PR_R] | other_pieces[PR_Q]);
   attacker |= bishop_attacks(occ, sq) & (other_pieces[PR_B] | other_pieces[PR_Q]);
   attacker |= knight_attacks[sq]      & (other_pieces[PR_N]);
-
-  // TODO: Pawn attacks
+  attacker |= other_pawn_attacks      & (1u << sq);
 
   return attacker != 0;
 }
 
 static bitboard king_attacks[NUM_SQUARES];
 static inline void
-generate_king_moves(bitboard own, bitboard other, bitboard other_pieces[6], square sq, piece_type types[NUM_SQUARES], irreversable_state meta, struct move_buffer *out)
+generate_king_moves(bitboard own, bitboard other, bitboard other_pieces[6], bitboard other_pawn_attacks, square sq, piece_type types[NUM_SQUARES], irreversable_state meta, struct move_buffer *out)
 {
   bitboard attacks = king_attacks[sq] & ~own;
   move_buffer_append_attacks(attacks, sq, types, out);
 
-  // TODO: castling
+  // is king checked?
+  if (is_square_checked(own, other, other_pieces, other_pawn_attacks, sq, types)) return;
 
+  // castling moves
+  bitboard castle_east = (1u << (sq + 2)),
+           castle_west = castle_east >> 4;
+  if (castle_east & meta.castling_rights && !is_square_checked(own, other, other_pieces, other_pawn_attacks, sq + 1, types))
+  {
+    move_buffer_append_move(sq, sq + 2, PT_NONE, MT_CASTLE_KING, out);
+  }
+  if (castle_west & meta.castling_rights && !is_square_checked(own, other, other_pieces, other_pawn_attacks, sq - 1, types))
+  {
+    move_buffer_append_move(sq, sq - 2, PT_NONE, MT_CASTLE_QUEEN, out);
+  }
 }
 
 static const bitboard rank_1 = 0x00000000000000FF;
@@ -132,26 +152,10 @@ static const bitboard h_file = 0x8080808080808080;
 static inline void
 move_buffer_append_promotions(square from, square to, piece_type capture, struct move_buffer *out)
 {
-  out->moves[out->size].from    = from;
-  out->moves[out->size].to      = to;
-  out->moves[out->size].capture = capture;
-  out->moves[out->size].type    = MT_PROMOTION_KNIGHT;
-  ++out->size;
-  out->moves[out->size].from    = from;
-  out->moves[out->size].to      = to;
-  out->moves[out->size].capture = capture;
-  out->moves[out->size].type    = MT_PROMOTION_BISHOP;
-  ++out->size;
-  out->moves[out->size].from    = from;
-  out->moves[out->size].to      = to;
-  out->moves[out->size].capture = capture;
-  out->moves[out->size].type    = MT_PROMOTION_ROOK;
-  ++out->size;
-  out->moves[out->size].from    = from;
-  out->moves[out->size].to      = to;
-  out->moves[out->size].capture = capture;
-  out->moves[out->size].type    = MT_PROMOTION_QUEEN;
-  ++out->size;
+  move_buffer_append_move(from, to, capture, MT_PROMOTION_KNIGHT, out);
+  move_buffer_append_move(from, to, capture, MT_PROMOTION_BISHOP, out);
+  move_buffer_append_move(from, to, capture, MT_PROMOTION_ROOK, out);
+  move_buffer_append_move(from, to, capture, MT_PROMOTION_QUEEN, out);
 }
 
 static inline void
@@ -168,20 +172,16 @@ generate_pawn_moves_white(bitboard own, bitboard other, bitboard pieces, piece_t
   bitboard en_passant_east = (meta.en_passant_potential >> 0x1) & pieces;
   if (en_passant_east) // en passant east
   {
-    out->moves[out->size].from    = log_bit(meta.en_passant_potential >> 0x1);
-    out->moves[out->size].to      = log_bit(meta.en_passant_potential << 0x8);
-    out->moves[out->size].capture = PT_NONE;
-    out->moves[out->size].type    = MT_EN_PASSANT;
-    ++out->size;
+    from = log_bit(meta.en_passant_potential >> 0x1);
+    to   = log_bit(meta.en_passant_potential << 0x8);
+    move_buffer_append_move(from, to, PT_NONE, MT_EN_PASSANT, out);
   }
   bitboard en_passant_west = (meta.en_passant_potential << 0x1) & pieces;
   if (en_passant_west) // en passant west
   {
-    out->moves[out->size].from    = log_bit(meta.en_passant_potential << 0x1);
-    out->moves[out->size].to      = log_bit(meta.en_passant_potential << 0x8);
-    out->moves[out->size].capture = PT_NONE;
-    out->moves[out->size].type    = MT_EN_PASSANT;
-    ++out->size;
+    from = log_bit(meta.en_passant_potential << 0x1);
+    to   = log_bit(meta.en_passant_potential << 0x8);
+    move_buffer_append_move(from, to, PT_NONE, MT_EN_PASSANT, out);
   }
 
   while (singles)
@@ -190,11 +190,7 @@ generate_pawn_moves_white(bitboard own, bitboard other, bitboard pieces, piece_t
     from = to >> 0x8;
     if (to & ~rank_8) // no promotion
     {
-      out->moves[out->size].from = from;
-      out->moves[out->size].to = to;
-      out->moves[out->size].capture = PT_NONE;
-      out->moves[out->size].type = MT_NORMAL;
-      ++out->size;
+      move_buffer_append_move(from, to, PT_NONE, MT_NORMAL, out);
     } else // promotion
     {
       move_buffer_append_promotions(from, to, PT_NONE, out);
@@ -204,11 +200,7 @@ generate_pawn_moves_white(bitboard own, bitboard other, bitboard pieces, piece_t
   {
     to = pop_bit(&doubles);
     from = to >> 0x10;
-    out->moves[out->size].from = from;
-    out->moves[out->size].to = to;
-    out->moves[out->size].capture = PT_NONE;
-    out->moves[out->size].type = MT_DOUBLE_PAWN;
-    ++out->size;
+    move_buffer_append_move(from, to, PT_NONE, MT_DOUBLE_PAWN, out);
   }
   while (east_captures)
   {
@@ -216,11 +208,7 @@ generate_pawn_moves_white(bitboard own, bitboard other, bitboard pieces, piece_t
     from = to >> 0x9;
     if (to & ~rank_8) // no promotion
     {
-      out->moves[out->size].from = from;
-      out->moves[out->size].to = to;
-      out->moves[out->size].capture = types[to];
-      out->moves[out->size].type = MT_NORMAL;
-      ++out->size;
+      move_buffer_append_move(from, to, types[to], MT_NORMAL, out);
     } else // promotion
     {
       move_buffer_append_promotions(from, to, types[to], out);
@@ -232,11 +220,7 @@ generate_pawn_moves_white(bitboard own, bitboard other, bitboard pieces, piece_t
     from = to >> 0x7;
     if (to & ~rank_8) // no promotion
     {
-      out->moves[out->size].from = from;
-      out->moves[out->size].to = to;
-      out->moves[out->size].capture = types[to];
-      out->moves[out->size].type = MT_NORMAL;
-      ++out->size;
+      move_buffer_append_move(from, to, types[to], MT_NORMAL, out);
     } else // promotion
     {
       move_buffer_append_promotions(from, to, types[to], out);
@@ -255,23 +239,19 @@ generate_pawn_moves_black(bitboard own, bitboard other, bitboard pieces, piece_t
 
   square from, to;
 
-  bitboard en_passant_east = (meta.en_passant_potential >> 0x1) & pieces;
+  bitboard en_passant_east = (meta.en_passant_potential >> 0x1) & ~a_file & pieces;
   if (en_passant_east) // en passant east
   {
-    out->moves[out->size].from    = log_bit(meta.en_passant_potential >> 0x1);
-    out->moves[out->size].to      = log_bit(meta.en_passant_potential >> 0x8);
-    out->moves[out->size].capture = PT_NONE;
-    out->moves[out->size].type    = MT_EN_PASSANT;
-    ++out->size;
+    from = log_bit(meta.en_passant_potential >> 0x1);
+    to   = log_bit(meta.en_passant_potential >> 0x8);
+    move_buffer_append_move(from, to, PT_NONE, MT_EN_PASSANT, out);
   }
-  bitboard en_passant_west = (meta.en_passant_potential << 0x1) & pieces;
+  bitboard en_passant_west = (meta.en_passant_potential << 0x1) & ~h_file & pieces;
   if (en_passant_west) // en passant west
   {
-    out->moves[out->size].from    = log_bit(meta.en_passant_potential << 0x1);
-    out->moves[out->size].to      = log_bit(meta.en_passant_potential >> 0x8);
-    out->moves[out->size].capture = PT_NONE;
-    out->moves[out->size].type    = MT_EN_PASSANT;
-    ++out->size;
+    from = log_bit(meta.en_passant_potential << 0x1);
+    to   = log_bit(meta.en_passant_potential >> 0x8);
+    move_buffer_append_move(from, to, PT_NONE, MT_EN_PASSANT, out);
   }
 
   while (singles)
@@ -280,11 +260,7 @@ generate_pawn_moves_black(bitboard own, bitboard other, bitboard pieces, piece_t
     from = to << 0x8;
     if (to & ~rank_1) // no promotion
     {
-      out->moves[out->size].from = from;
-      out->moves[out->size].to = to;
-      out->moves[out->size].capture = PT_NONE;
-      out->moves[out->size].type = MT_NORMAL;
-      ++out->size;
+      move_buffer_append_move(from, to, PT_NONE, MT_NORMAL, out);
     } else // promotion
     {
       move_buffer_append_promotions(from, to, PT_NONE, out);
@@ -294,11 +270,7 @@ generate_pawn_moves_black(bitboard own, bitboard other, bitboard pieces, piece_t
   {
     to = pop_bit(&doubles);
     from = to << 0x10;
-    out->moves[out->size].from = from;
-    out->moves[out->size].to = to;
-    out->moves[out->size].capture = PT_NONE;
-    out->moves[out->size].type = MT_DOUBLE_PAWN;
-    ++out->size;
+    move_buffer_append_move(from, to, PT_NONE, MT_DOUBLE_PAWN, out);
   }
   while (east_captures)
   {
@@ -306,11 +278,7 @@ generate_pawn_moves_black(bitboard own, bitboard other, bitboard pieces, piece_t
     from = to << 0x7;
     if (to & ~rank_1) // no promotion
     {
-      out->moves[out->size].from = from;
-      out->moves[out->size].to = to;
-      out->moves[out->size].capture = types[to];
-      out->moves[out->size].type = MT_NORMAL;
-      ++out->size;
+      move_buffer_append_move(from, to, types[to], MT_NORMAL, out);
     } else // promotion
     {
       move_buffer_append_promotions(from, to, types[to], out);
@@ -322,11 +290,7 @@ generate_pawn_moves_black(bitboard own, bitboard other, bitboard pieces, piece_t
     from = to << 0x9;
     if (to & ~rank_1) // no promotion
     {
-      out->moves[out->size].from = from;
-      out->moves[out->size].to = to;
-      out->moves[out->size].capture = types[to];
-      out->moves[out->size].type = MT_NORMAL;
-      ++out->size;
+      move_buffer_append_move(from, to, types[to], MT_NORMAL, out);
     } else // promotion
     {
       move_buffer_append_promotions(from, to, types[to], out);
@@ -334,13 +298,6 @@ generate_pawn_moves_black(bitboard own, bitboard other, bitboard pieces, piece_t
   }
 }
 
-
-#define GENERATE_ALL_MOVES(PT, own, other, pieces, types, out) \
-  while (pieces) \
-{ \
-  square from = pop_bit(&pieces); \
-  generate_## PT ##_moves(own, other, from, types, out); \
-}
 
 size_t
 generate_moves(board_state *board, irreversable_state meta, piece_type color_own, piece_type color_other, struct move_buffer *out)
@@ -350,6 +307,13 @@ generate_moves(board_state *board, irreversable_state meta, piece_type color_own
   bitboard own_union = own[PR_P] | own[PR_N] | own[PR_B] | own[PR_R] | own[PR_Q] | own[PR_K];
   bitboard other_union = other[PR_P] | other[PR_N] | other[PR_B] | other[PR_R] | other[PR_Q] | other[PR_K];
 
+#define GENERATE_ALL_MOVES(PT, own, other, pieces, types, out) \
+  while (pieces) \
+  { \
+    square from = pop_bit(&pieces); \
+    generate_## PT ##_moves(own, other, from, types, out); \
+  }
+
   // generate sliding moves
   GENERATE_ALL_MOVES(bishop, own_union, other_union, own[PR_B], board->types, out);
   GENERATE_ALL_MOVES(rook, own_union, other_union, own[PR_R], board->types, out);
@@ -357,15 +321,26 @@ generate_moves(board_state *board, irreversable_state meta, piece_type color_own
 
   GENERATE_ALL_MOVES(knight, own_union, other_union, own[PR_N], board->types, out);
 
-  if (color_other + PR_P == PT_BP) // white pawn moves
+#undef GENERATE_ALL_MOVES
+
+  bitboard other_pawn_attacks;
+  if (color_other + PR_P == PT_BP) // white's move
+  {
     generate_pawn_moves_white(own_union, other_union, board->bitboards[PT_WP], board->types, meta, out);
-  else // black pawn moves
+    other_pawn_attacks  = (other[PR_P] >> 9) & ~h_file;
+    other_pawn_attacks |= (other[PR_P] >> 7) & ~a_file;
+    generate_king_moves(own_union, other_union, other, other_pawn_attacks, log_bit(own[PR_K]), board->types, meta, out);
+  }
+  else // black's move
+  {
     generate_pawn_moves_black(own_union, other_union, board->bitboards[PT_BP], board->types, meta, out);
+    other_pawn_attacks  = (other[PR_P] << 7) & ~h_file;
+    other_pawn_attacks |= (other[PR_P] << 9) & ~a_file;
+    generate_king_moves(own_union, other_union, other, other_pawn_attacks, log_bit(own[PR_K]), board->types, meta, out);
+  }
 
   return out->size;
 }
-
-#undef GENERATE_ALL_MOVES
 
 struct move_buffer *
 move_buffer_create(size_t max_ply)
