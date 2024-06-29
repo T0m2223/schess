c20d73665767297478ebfe080d7cba05b865c19a
kiliac03 <kiliac03@mi.fu-berlin.de>
2024-06-29 23:36:45 +0200

    negamax, board_state -> game_state

--- schess/search.c
+++ schess/search.c
@@ -5,16 +5,11 @@
 
 int quiesce(int alpha, int beta)
 {
-  return 1;
+  return 0;
 }
 
 int
-alpha_beta_min(board_state *board, irreversable_state meta, int alpha, int beta, unsigned depth, struct move_buffer *mbuf);
-int
-alpha_beta_min(board_state *board, irreversable_state meta, int alpha, int beta, unsigned depth, struct move_buffer *mbuf) { return 1; }
-
-int
-alpha_beta_max(board_state *board, irreversable_state meta, int alpha, int beta, unsigned depth, struct move_buffer *mbuf)
+alpha_beta(game_state *game, irreversable_state meta, int alpha, int beta, unsigned depth, struct move_buffer *mbuf)
 {
   if (!depth) return quiesce(alpha, beta);
 
@@ -22,17 +17,17 @@ alpha_beta_max(board_state *board, irreversable_state meta, int alpha, int beta,
   int score;
   irreversable_state meta_copy;
 
-  num_moves = generate_moves(board, meta, PT_WP, PT_BP, &mbuf[depth]);
+  num_moves = generate_moves(game, meta, &mbuf[depth]);
 
   for (i = 0; i < num_moves; ++i)
   {
     // move ordering
     move *m = mbuf[depth].moves + i;
-
     meta_copy = meta;
-    move_make(m, board, &meta_copy);
-    score = -alpha_beta_min(board, meta_copy, alpha, beta, depth - 1, mbuf);
-    move_unmake(m, board);
+
+    move_make(m, game, &meta_copy);
+    score = -alpha_beta(game, meta_copy, -beta, -alpha, depth - 1, mbuf);
+    move_unmake(m, game);
 
     if (score >= beta) return beta;
     if (score > alpha) alpha = score;
