7d9a327d7b067a08d287be4ac18d513b52aa6b9f
kiliac03 <kiliac03@mi.fu-berlin.de>
2024-06-30 21:30:47 +0200

    remove tests

--- schess/schess.c
+++ schess/schess.c
@@ -7,25 +7,6 @@
 
 int main(void)
 {
-  board_state board;
-  irreversable_state meta;
-
-
-  parse_FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", &board, &meta);
-  printf("Board read in:\n");
-  print_board(&board);
-
-
-  struct move_buffer *mbuf = move_buffer_create(1);
-  move_gen_init_LUTs();
-  generate_moves(&board, meta, PT_WP, PT_BP, mbuf);
-  printf("Possible moves:\n");
-  print_moves(&board, mbuf);
-
-
-  move_make(mbuf->moves, &board, &meta);
-  printf("After making move 0:\n");
-  print_board(&board);
-
+  printf("SCHESS ENGINE by Kilian Chung\n");
   return EXIT_SUCCESS;
 }
