4a7be55ca992cb6c24d79b269b2725370ab3738f
kiliac03 <kiliac03@mi.fu-berlin.de>
2024-06-30 21:27:58 +0200

    fixed ep castling many bugs

--- schess/move.c
+++ schess/move.c
@@ -33,6 +33,8 @@ move_make(move *m, game_state *game, irreversable_state *meta)
 
   if (capture != PT_NONE) meta->halfmove_clock = 0;
 
+  game->en_passant_potential = 0ull;
+
   piece_type promo_type, castle_rook;
 
   switch (m->type)
@@ -41,18 +43,20 @@ move_make(move *m, game_state *game, irreversable_state *meta)
     if (piece == PT_WP || piece == PT_BP) meta->halfmove_clock = 0;
     break;
   case MT_DOUBLE_PAWN:
-    game->en_passant_potential = sq2bb(m->to) & 0x000000FFFF000000;
+    game->en_passant_potential = sq2bb(m->to);
     meta->halfmove_clock = 0;
     break;
   case MT_EN_PASSANT:
     if (piece == PT_WP)
     {
-      bitboard_unset(m->to + 8, &board->bitboards[PT_BP]);
+      bitboard_unset(m->to - 8, &board->bitboards[PT_BP]);
+      board->types[m->to - 8] = PT_NONE;
       value += eval_piece_value(PT_BP);
     }
     else // piece = PT_BP
     {
-      bitboard_unset(m->to - 8, &board->bitboards[PT_WP]);
+      bitboard_unset(m->to + 8, &board->bitboards[PT_WP]);
+      board->types[m->to + 8] = PT_NONE;
       value += eval_piece_value(PT_WP);
     }
     meta->halfmove_clock = 0;
@@ -60,19 +64,22 @@ move_make(move *m, game_state *game, irreversable_state *meta)
 
   case MT_CASTLE_KING:
     castle_rook = piece + (PR_R - PR_K);
-    bitboard_unset(m->from | 0x3, &board->bitboards[castle_rook]);
-    bitboard_set(m->to - 1, &board->bitboards[castle_rook]);
+    bitboard_unset(m->to + 1, &board->bitboards[castle_rook]);
+    bitboard_set(m->from + 1, &board->bitboards[castle_rook]);
+    board->types[m->to + 1] = PT_NONE;
+    board->types[m->from + 1] = castle_rook;
     break;
   case MT_CASTLE_QUEEN:
     castle_rook = piece + (PR_R - PR_K);
-    bitboard_unset(m->from & ~0x3, &board->bitboards[castle_rook]);
-    bitboard_set(m->to + 1, &board->bitboards[castle_rook]);
+    bitboard_unset(m->to - 2, &board->bitboards[castle_rook]);
+    bitboard_set(m->from - 1, &board->bitboards[castle_rook]);
+    board->types[m->to - 2] = PT_NONE;
+    board->types[m->from - 1] = castle_rook;
     break;
 
 #define MOVE_MAKE_HANDLE_PROMOTION(rel_type) \
     { \
       promo_type = piece + (rel_type - PR_P); \
-      bitboard_set(m->to, &board->bitboards[promo_type]); \
       value += eval_piece_value(promo_type) - eval_piece_value(piece); \
       meta->halfmove_clock = 0; \
       piece = promo_type; \
@@ -88,27 +95,29 @@ move_make(move *m, game_state *game, irreversable_state *meta)
   // unset castle rights if rook is taken
   switch (m->to)
   {
-  case 0x07: meta->castling_rights &= 0xFF0000000000000F; break;
-  case 0x00: meta->castling_rights &= 0xFF000000000000F0; break;
-  case 0x3F: meta->castling_rights &= 0x0F000000000000FF; break;
-  case 0x37: meta->castling_rights &= 0xF0000000000000FF; break;
+  case h1: meta->castling_rights &= 0xFF0000000000000F; break;
+  case a1: meta->castling_rights &= 0xFF000000000000F0; break;
+  case h8: meta->castling_rights &= 0x0F000000000000FF; break;
+  case a8: meta->castling_rights &= 0xF0000000000000FF; break;
   default: break;
   }
 
   switch (m->from)
   {
-  case 0x07: meta->castling_rights &= 0xFF0000000000000F; break; // white rook kingside moved
-  case 0x00: meta->castling_rights &= 0xFF000000000000F0; break; // white rook queenside moved
-  case 0x3F: meta->castling_rights &= 0x0F000000000000FF; break; // black rook kingside moved
-  case 0x37: meta->castling_rights &= 0xF0000000000000FF; break; // black rook queenside moved
-  case 0x04: meta->castling_rights &= 0xFF00000000000000; break; // white king moved
-  case 0x3C: meta->castling_rights &= 0x00000000000000FF; break; // black king moved
+  case h1: meta->castling_rights &= 0xFF0000000000000F; break; // white rook kingside moved
+  case a1: meta->castling_rights &= 0xFF000000000000F0; break; // white rook queenside moved
+  case h8: meta->castling_rights &= 0x0F000000000000FF; break; // black rook kingside moved
+  case a8: meta->castling_rights &= 0xF0000000000000FF; break; // black rook queenside moved
+  case e1: meta->castling_rights &= 0xFF00000000000000; break; // white king moved
+  case e8: meta->castling_rights &= 0x00000000000000FF; break; // black king moved
   default: break;
   }
 
   // set board
   bitboard_set(m->to, &board->bitboards[piece]);
   board->types[m->to] = piece;
+
+  game->active = OTHER_COLOR(game->active);
 
   return value;
 }
@@ -138,31 +147,36 @@ move_unmake(move *m, game_state *game)
   case MT_EN_PASSANT:
     if (piece == PT_WP)
     {
-      bitboard_set(m->to + 8, &board->bitboards[PT_BP]);
+      bitboard_set(m->to - 8, &board->bitboards[PT_BP]);
+      board->types[m->to - 8] = PT_BP;
       value += eval_piece_value(PT_BP);
     }
     else // piece = PT_BP
     {
-      bitboard_set(m->to - 8, &board->bitboards[PT_WP]);
+      bitboard_set(m->to + 8, &board->bitboards[PT_WP]);
+      board->types[m->to + 8] = PT_WP;
       value += eval_piece_value(PT_WP);
     }
     break;
 
   case MT_CASTLE_KING:
     castle_rook = piece + (PR_R - PR_K);
-    bitboard_unset(m->to - 1, &board->bitboards[castle_rook]);
-    bitboard_set(m->from | 0x3, &board->bitboards[castle_rook]); // TODO: probably wrong
+    bitboard_unset(m->from + 1, &board->bitboards[castle_rook]);
+    bitboard_set(m->to + 1, &board->bitboards[castle_rook]);
+    board->types[m->to + 1] = castle_rook;
+    board->types[m->from + 1] = PT_NONE;
     break;
   case MT_CASTLE_QUEEN:
     castle_rook = piece + (PR_R - PR_K);
-    bitboard_unset(m->to + 1, &board->bitboards[castle_rook]);
-    bitboard_set(m->from & ~0x3, &board->bitboards[castle_rook]); // TODO: probably wrong
+    bitboard_unset(m->from - 1, &board->bitboards[castle_rook]);
+    bitboard_set(m->to - 2, &board->bitboards[castle_rook]);
+    board->types[m->to - 2] = castle_rook;
+    board->types[m->from - 1] = PT_NONE;
     break;
 
 #define MOVE_UNMAKE_HANDLE_PROMOTION(rel_type) \
     { \
       prepromo_type = piece + (PR_P - rel_type); \
-      bitboard_unset(m->to, &board->bitboards[piece]); \
       value += eval_piece_value(prepromo_type) - eval_piece_value(piece); \
       piece = prepromo_type; \
     }
@@ -175,6 +189,8 @@ move_unmake(move *m, game_state *game)
 
   bitboard_set(m->from, &board->bitboards[piece]);
   board->types[m->from] = piece;
+
+  game->active = OTHER_COLOR(game->active);
 
   return -value;
 }
